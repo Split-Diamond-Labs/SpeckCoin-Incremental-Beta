497a6b87ca068a65e9b3ced3f651b58f53a1c802
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-05-03 11:19:53 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { Core } from "./modules/coreFunctions.js";
 let timePassed = 0;
 function update() {
   timePassed++;
+  console.log("Update");
   if (timePassed < 120) {
     Core.showById("loader");
     Core.hideById("pageContent");
