67c702a3c1ab07e39139555695bdb1685edb19f5
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-05-04 12:56:07 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // CORE FUNCTIONS
 
-static notify(text) {
+function notify(text) {
   alert(text);
 }
-static hideById(id) {
+function hideById(id) {
   document.getElementById(id).style.display = "none";
 }
-static showById(id) {
+function showById(id) {
   document.getElementById(id).style.display = "block";
 }
-static randomElement(array) {
+function randomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-static romanize(num) {
+function romanize(num) {
   var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
   for ( i in lookup ) {
     while ( num >= lookup[i] ) {
@@ -24,11 +24,11 @@ static romanize(num) {
   return roman;
 }
 
-static saveGame() {
+function saveGame() {
   localStorage.setItem("local_game_saved",true)
 }
 
-static loadGame() {
+function loadGame() {
   if (localStorage.getItem("local_game_saved") != "true") {
     console.log("No save was found, starting new game...")
     return
