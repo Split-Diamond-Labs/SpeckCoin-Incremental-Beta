1a692376b29086b27a1a08ff2c1c447ab1561647
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-05-16 11:34:38 +1000

    Create resource.js

---
+++ js/resource.js
@@ -0,0 +1,20 @@
+// Resource class
+class Resource {
+  constructor(amounts, basePrices) {
+    this.amounts = amounts;
+    this.basePrices = basePrices;
+  }
+
+  buyBuilding(lvl) {
+    if (!((this.basePrices[lvl - 1] * Math.pow(1.5, this.amounts[lvl])) > this.amounts[0])) {
+      this.amounts[0] -= this.basePrices[lvl - 1] * Math.pow(1.5, this.amounts[lvl]);
+      this.amounts[lvl]++;
+    } else {
+      Core.notify("You cannot afford this!");
+    }
+  }
+
+  cost(lvl) {
+    return this.basePrices[lvl - 1] * Math.pow(1.5, this.amounts[lvl]);
+  }
+}
