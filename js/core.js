7a471ac21f5f9babe90905b317fa700235420b4e
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-06-17 18:40:13 +1000

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,18 +1,10 @@
 // CORE FUNCTIONS
 class Core {
+  
   static notify(text) {
     const digitsHex = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
     document.getElementById("notify").innerHTML = text;
-    var time = 0;
-    var i = 0;
-    var j = 0;
-    for ( ; j < 16 ; ) {
-      for ( ; i < 16 ; ) {
-        setTimeout(function(){document.getElementById("notify").style.color = "#ff" + digitsHex[j] + "" + digitsHex[i] + "" + digitsHex[j] + "" + digitsHex[i];}, time);
-        time++
-      }
-      j++;
-    }
+    startFadeDec(255, 0, 0, 255, 255, 255, 256, "notify");
   }
   static hideById(id) {
     document.getElementById(id).style.display = "none";
@@ -77,3 +69,142 @@ class Core {
   }
 
 }
+var numSteps=0;
+var startingRed=0;
+var startingGreen=0;
+var startingBlue=0;
+var endingRed=0;
+var endingGreen=0;
+var endingBlue=0;
+var deltaRed=0;
+var deltaGreen=0;
+var deltaBlue=0;
+var currentRed=0;
+var currentGreen=0;
+var currentBlue=0;
+var currentStep=0;
+var timerID=0;
+
+////////////////////////////////////////
+// fade timer
+////////////////////////////////////////
+function startFadeDec(startR, startG, startB, 
+   endR, endG, endB, nSteps, id)
+{
+//alert("sf");
+	// need to parse, otherwise it thinks it's not a number
+  	currentRed=startingRed=parseInt(startR, 10);
+  	currentGreen=startingGreen=parseInt(startG, 10);
+  	currentBlue=startingBlue=parseInt(startB, 10);
+  	endingRed=parseInt(endR, 10);
+  	endingGreen=parseInt(endG, 10);
+  	endingBlue=parseInt(endB, 10);
+  	numSteps=parseInt(nSteps, 10);
+  	deltaRed=(endingRed-startingRed)/numSteps;
+  	deltaGreen=(endingGreen-startingGreen)/numSteps;
+	deltaBlue=(endingBlue-startingBlue)/numSteps;
+	currentStep=0;
+	
+/*	alert("cr="+currentRed+" cg="+currentGreen+" cb="+currentBlue);
+	alert("dr="+deltaRed+" dg="+deltaGreen+" db="+deltaBlue);
+	alert("er="+endingRed+" eg="+endingGreen+" eb="+endingBlue);
+*/	
+  	fade(id);
+}
+  
+////////////////////////////////////////
+// fade timer
+////////////////////////////////////////
+function fade(id)
+{
+//	alert(color);
+//  	alert(document.bgColor);
+  	
+  	currentStep++;
+  	// if not done yet, change the backround
+  	if (currentStep<=numSteps)
+  	{
+		// convert to hex	
+		var hexRed=decToHex(currentRed);
+		var hexGreen=decToHex(currentGreen);
+		var hexBlue=decToHex(currentBlue);
+	
+		var color="#"+hexRed+""+hexGreen+""+hexBlue+"";
+//	alert(color);
+		
+	  	document.getElementById(id).style.backgroundColor=color;
+//  	alert(document.bgColor);
+
+		// increment color
+		currentRed+=deltaRed;
+		currentGreen+=deltaGreen;
+		currentBlue+=deltaBlue;
+//	alert("cr="+currentRed+" cg="+currentGreen+" cb="+currentBlue);
+		
+	  	timerID=setTimeout("fade()", 1); // sets timer so that this function will
+                  		   		      // be called every 10 miliseconds
+   }
+}
+
+////////////////////////////////////////
+// convert decimal to hexadecimal number
+////////////////////////////////////////
+function decToHex(decNum)
+{
+//alert ("1");
+	decNum=Math.floor(decNum);
+	var decString=""+decNum;
+	// make sure the number is valid
+	for (var i=0; i<decString.length; i++)
+	{
+//alert ("2");
+	
+		if (decString.charAt(i)>='0' && decString.charAt(i)<='9')
+		{
+		}
+		else
+		{
+			alert(decString+" is not a valid decimal number because it contains "+decString.charAt(i));
+ 			return decNum;
+		}
+	}
+	var result=decNum;
+	var remainder="";
+	// use string because math operation won't work with hex alphabet
+	var hexNum="";
+
+	var hexAlphabet=new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
+//	alert("converting "+decNum+" to "+hexNum);
+	while (result>0)
+	{
+		result=Math.floor(decNum/16);
+		remainder=decNum%16;
+		decNum=result;
+
+/*		if (remainder>=10)
+		{
+			// use double quotes because Netscape 3 will give error if using single quote
+			if (remainder==10)
+				remainder="A";
+			if (remainder==11)
+				remainder="B";
+			if (remainder==12)
+				remainder="C";
+			if (remainder==13)
+				remainder="D";
+			if (remainder==14)
+				remainder="E";
+			if (remainder==15)
+				remainder="F";
+		}*/
+		// just append the next remainder to the beginning of the string
+		hexNum=""+hexAlphabet[remainder]+""+hexNum;
+	};
+//	alert("converting "+decNum+" to "+hexNum);
+	// make sure to have at least 2 digits
+	if (hexNum.length==1)
+		hexNum="0"+hexNum;
+	else if (hexNum.length==0)
+		hexNum="00";
+	return hexNum;
+}   
