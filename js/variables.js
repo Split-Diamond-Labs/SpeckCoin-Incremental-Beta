e4fcff306f40de8c184702f5d5ef51440f684289
Flyspeck101 <yeetersdeleters12345@gmail.com>
2021-11-20 16:23:15 +1100

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,86 +1,8 @@
 const dataObject = {
-<<<<<<< Updated upstream
-    "coins": 100,
-    "coinBuildings": {
-        "costFactor": 1.1,
-
-        "tier1": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 100,
-            "multiplier": 1
-        },
-
-        "tier2": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 2000,
-            "multiplier": 1
-        },
-
-        "tier3": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 40000,
-            "multiplier": 1
-        },
-
-        "tier4": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 800000,
-            "multiplier": 1
-        },
-
-        "tier5": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 16000000,
-            "multiplier": 1
-        }
-    },
-
-    "diamonds": 0,
-    "diamondBuildings": {
-        "costFactor": 1.5,
-
-        "tier1": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 1,
-            "multiplier": 1
-        },
-
-        "tier2": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 10,
-            "multiplier": 1
-        },
-
-        "tier3": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 100,
-            "multiplier": 1
-        },
-
-        "tier4": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 1000,
-            "multiplier": 1
-        },
-        
-        "tier5": {
-            "owned": 0,
-            "produced": 0,
-            "cost": 10000,
-            "multiplier": 1
-=======
     "speckcoin": {
         "name": "SpeckCoin",
         "owned": 100,
+        "costFactor": 1.1,
         "buildings": {
             "tier1": {
                 "name": "Flyspeck Clone",
@@ -149,7 +71,6 @@ const dataObject = {
                 "produced": 0,
                 "multiplier": 1
             }
->>>>>>> Stashed changes
         }
     }
 };
