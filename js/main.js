2c4b05b80fc143b2f05b86aa52dcb303cd30ce6b
Flyspeck101 <yeetersdeleters12345@gmail.com>
2021-11-20 16:56:16 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+setInterval(() => {
+  // Updates
+  refresh();
+}, 10);
 
-
+setInterval(() => {
+  // Buildings 
+  $_$["speckCoin"]["owned"] += Math.floor($_$["speckCoin"]["buildings"]["tier1"]["owned"]) * $_$["speckCoin"]["buildings"]["tier1"]["baseRate"] * $_$["speckCoin"]["buildings"]["tier1"]["multiplier"] / 10;
+  for (i = 2; i <= 5; i++) {
+    $_$["speckCoin"]["buildings"]["tier" + (i - 1)]["owned"] += Math.floor($_$["speckCoin"]["buildings"]["tier" + i]["owned"]) * $_$["speckCoin"]["buildings"]["tier" + i]["baseRate"] * $_$["speckCoin"]["buildings"]["tier" + i]["multiplier"] / 10;
+  }
+}, 100);
