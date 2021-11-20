5d035df2fb23a7463630df14bf607f69965a2034
Flyspeck101 <yeetersdeleters12345@gmail.com>
2021-11-20 17:12:21 +1100

    Update core.js

--- js/core.js
+++ js/core.js
@@ -76,7 +76,7 @@ function reset() {
 function refresh() {
     for (let i = 1; i <= 5; i++) {
         document.getElementById("CBA" + i).innerText = Math.floor($_$["speckCoin"]["buildings"]["tier" + i]["owned"]);
-        document.getElementById("CBC" + i).innerText = $_$["speckCoin"]["buildings"]["tier" + i]["cost"];
+        document.getElementById("CBC" + i).innerText = Math.floor($_$["speckCoin"]["buildings"]["tier" + i]["cost"]);
         document.getElementById("CBP" + i).innerText = $_$["speckCoin"]["buildings"]["tier" + i]["baseRate"] * Math.floor($_$["speckCoin"]["buildings"]["tier" + i]["owned"]) * $_$["speckCoin"]["buildings"]["tier" + i]["multiplier"];
     }
     document.getElementById("speckcoin-value").innerText = Math.floor($_$["speckCoin"]["owned"]);
